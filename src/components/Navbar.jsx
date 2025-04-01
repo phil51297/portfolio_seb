f1441309de672ca55f1254e8e1c7c2117cd058f9
phil51297 <b.philippe.97@gmail.com>
2025-04-01 23:39:48 +0200

    init commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+    return (
+        <nav className="absolute top-0 w-full z-10">
+            <div className="max-w-5xl mx-auto px-4">
+                <div className="flex justify-between items-center h-16">
+                    <a
+                        href="#about"
+                        className="font-extrabold text-[24px] text-[#F43F5E] font-inter"
+                    >
+                        Sebastien Bonnafous
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
+};
