a8e634f26adba0e005054e2d66c6202fffbe30e1
phil51297 <b.philippe.97@gmail.com>
2025-04-02 00:40:25 +0200

    feat: added menu and mobile menu with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
 function App() {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <>
-            <Navbar />
+            <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         </>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,9 @@
-export const Navbar = () => {
+import { useEffect } from 'react';
+
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
+    useEffect(() => {
+        document.body.style.overflow = menuOpen ? 'hidden' : '';
+    }, [menuOpen]);
     return (
         <nav className="absolute top-0 w-full z-10">
             <div className="max-w-5xl mx-auto px-4">
@@ -9,6 +14,42 @@ export const Navbar = () => {
                     >
                         Sebastien Bonnafous
                     </a>
+                    <div
+                        className="w-7 h-5 relative cursor-pointer z-10 md:hidden"
+                        onClick={() => setMenuOpen((prev) => !prev)}
+                    >
+                        &#9776;
+                    </div>
+
+                    <div className="hidden md:flex items-center space-x-8 ">
+                        <a
+                            href="about"
+                            className="text-[#F43F5E] hove:text-white transition-colors"
+                        >
+                            About
+                        </a>
+
+                        <a
+                            href="skills"
+                            className="text-[#F43F5E] hove:text-white transition-colors"
+                        >
+                            Skills
+                        </a>
+
+                        <a
+                            href="projects"
+                            className="text-[#F43F5E] hove:text-white transition-colors"
+                        >
+                            Projects
+                        </a>
+
+                        <a
+                            href="contact"
+                            className="text-[#F43F5E] hove:text-white transition-colors"
+                        >
+                            Contact
+                        </a>
+                    </div>
                 </div>
             </div>
         </nav>
