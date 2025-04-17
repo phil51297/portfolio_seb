9ef5e640fb3ed6540f9f30be37ddfc179c3702ae
phil51297 <b.philippe.97@gmail.com>
2025-04-17 14:26:05 +0200

    feat: remove onHover effect starting from table size to smaller window

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -1,17 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
 import { loadSlim } from '@tsparticles/slim';
 import particlesConfig from './config/particles.json';
 
 export const ParticlesBackground = () => {
     const [init, setInit] = useState(false);
+    const [config, setConfig] = useState(particlesConfig);
+    const isSmallScreen = useRef(window.innerWidth <= 768);
 
     useEffect(() => {
+        // Initialize particles engine
         initParticlesEngine(async (engine) => {
             await loadSlim(engine);
         }).then(() => {
             setInit(true);
         });
+
+        // Update particles config based on screen size
+        const updateConfig = () => {
+            const isNowSmallScreen = window.innerWidth <= 768;
+
+            // Only update if the screen size crosses the threshold
+            if (isNowSmallScreen !== isSmallScreen.current) {
+                isSmallScreen.current = isNowSmallScreen;
+
+                const updatedConfig = { ...particlesConfig };
+                updatedConfig.interactivity.events.onHover.enable =
+                    !isNowSmallScreen;
+                setConfig(updatedConfig);
+            }
+        };
+
+        // Add event listener for screen resize
+        window.addEventListener('resize', updateConfig);
+
+        return () => {
+            window.removeEventListener('resize', updateConfig);
+        };
     }, []);
 
     const particlesLoaded = (container) => {
@@ -22,7 +47,7 @@ export const ParticlesBackground = () => {
         return (
             <Particles
                 id="tsparticles"
-                options={particlesConfig}
+                options={config}
                 particlesLoaded={particlesLoaded}
             />
         );
