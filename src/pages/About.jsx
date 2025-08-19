114c833a8e484a02662c7a964b531d89a6d9d86e
phil51297 <b.philippe.97@gmail.com>
2025-08-19 12:23:49 +0200

    changed file name when downloaded

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -66,7 +66,7 @@ export const About = () => {
                     <div className="flex justify-start mt-12 max-w-3xs">
                         <a className="bg-[#F43F5E] py-2 px-5 text-white font-bold font-[Poppins] rounded-lg shadow-md hover:shadow-red-400 hover:shadow-[0_0_10px_3px] transition hover:-translate-y-0.5 cursor-pointer"
                         href='/assets/files/cvseb.pdf'
-                        download='cvseb.pdf'>
+                        download='CV_Sebastien_Bonnafous.pdf'>
                             Download CV
                         </a>
                     </div>
